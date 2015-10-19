d15db0c96d7be02c76d7d5c7a01a16fcd5f19d4c
Nicolas, Bertrand <tramber301@gmail.com>
2015-10-19 11:49:58 +0200

    Fix issue with update to ubuntu 15.04
    With the update to ubuntu 15.04 this script stopped working,
    terminal were closed just after being launched.
    (gnome version 3.14)
    
    - Using bash interactive terminal option when launching terminals
    fixed the issue.
    - Disable StartupNotification from .desktop file

--- terminal_class.sh
+++ terminal_class.sh
@@ -1,7 +1,16 @@
-#!/bin/bash
+#!/bin/bash -i
 
-CUSTOM_PROFILE="FIRST"
+# Define CUSTOM_PROFILE in order to open terminals with the related profile
+#CUSTOM_PROFILE="FIRST"
+CUSTOM_PROFILE=""
 
-/usr/bin/gnome-terminal --profile=$CUSTOM_PROFILE --title=FIRST --working-directory=$PWD --geometry=104x55+0+0
-/usr/bin/gnome-terminal --profile=$CUSTOM_PROFILE --title=SECOND --working-directory=$PWD --geometry=105x28+960+0
-/usr/bin/gnome-terminal --profile=$CUSTOM_PROFILE --title=THIRD --working-directory=$PWD --geometry=105x23+960+595
+
+if [ -z $CUSTOM_PROFILE ]; then
+	/usr/bin/gnome-terminal --title=FIRST --working-directory=$PWD --geometry=104x55+0+0 
+	/usr/bin/gnome-terminal --title=SECOND --working-directory=$PWD --geometry=105x28+960+0
+	/usr/bin/gnome-terminal --title=THIRD --working-directory=$PWD --geometry=105x23+960+595
+else
+	/usr/bin/gnome-terminal --profile=$CUSTOM_PROFILE --title=FIRST --working-directory=$PWD --geometry=104x55+0+0
+	/usr/bin/gnome-terminal --profile=$CUSTOM_PROFILE --title=SECOND --working-directory=$PWD --geometry=105x28+960+0
+	/usr/bin/gnome-terminal --profile=$CUSTOM_PROFILE --title=THIRD --working-directory=$PWD --geometry=105x23+960+595
+fi
