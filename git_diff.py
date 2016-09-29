318344008e85bac3a3cafa1d08773fad6d10b89a
Nicolas Bertrand <tramber30@gmail.com>
2016-09-29 12:36:32 +0200

    git_diff.py: python3 compatibility

--- git_diff.py
+++ git_diff.py
@@ -6,8 +6,10 @@
 #        external = ~/git_diff.py
 #
 
+# compatibility workaround for python2.x
+from __future__ import print_function
 import sys
 import os
 
-print 'Diff on ' + sys.argv[5]
+print("Diff on", sys.argv[5])
 os.system('/usr/bin/meld "%s" "%s" 2>/dev/null' % (sys.argv[2], sys.argv[5]))
