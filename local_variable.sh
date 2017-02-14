34aabf455a3c61a2ec2963ae7d26a98ae9f25d17
Nicolas <$EMAIL>
2017-02-14 16:31:56 +0100

    Use local file to define useful path
    Dummy version added, customize it as necessary

---
+++ local_variable.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+# utility.sh path
+UTILITY_PATH="~/git/script/utility.sh"
+
+# DEBUG_LEVEL set debug level
+# used in utility.sh
+DEBUG_LEVEL=0
+
+# KEY_PATH basicaly your ~/.ssh folder
+# used in add_ssh_key.sh
+KEY_PATH="~/.ssh"
+
+# Terminal color scheme
+RED='\033[0;31m'
+NC='\033[0m'
